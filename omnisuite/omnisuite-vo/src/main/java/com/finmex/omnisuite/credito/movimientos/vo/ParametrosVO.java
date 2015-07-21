309aed714702203eae0d313086d660a42b19d55f
Uriel Sánchez <uriel.sanchez@planetmedia.com..mx>
2015-07-21 15:52:40 -0500

    Cambio de tipo en los campos del POJO

--- omnisuite/omnisuite-vo/src/main/java/com/finmex/omnisuite/credito/movimientos/vo/ParametrosVO.java
+++ omnisuite/omnisuite-vo/src/main/java/com/finmex/omnisuite/credito/movimientos/vo/ParametrosVO.java
@@ -1,32 +1,44 @@
 package com.finmex.omnisuite.credito.movimientos.vo;
 
+import com.finmex.omnisuite.client.Parametros;
+
 public class ParametrosVO {
-    private String numCliente;
-    private String folioExpediente;
+    private Long numCliente;
+    private Long folioExpediente;
     
 	/**
 	 * @return the numCliente
 	 */
-	public String getNumCliente() {
+	public Long getNumCliente() {
 		return numCliente;
 	}
 	/**
 	 * @param numCliente the numCliente to set
 	 */
-	public void setNumCliente(String numCliente) {
+	public void setNumCliente(Long numCliente) {
 		this.numCliente = numCliente;
 	}
 	/**
 	 * @return the folioExpediente
 	 */
-	public String getFolioExpediente() {
+	public Long getFolioExpediente() {
 		return folioExpediente;
 	}
 	/**
 	 * @param folioExpediente the folioExpediente to set
 	 */
-	public void setFolioExpediente(String folioExpediente) {
+	public void setFolioExpediente(Long folioExpediente) {
 		this.folioExpediente = folioExpediente;
 	}
+	
+	private static Parametros toParametros(ParametrosVO vo) {
+		Parametros parametros = new Parametros();
+		parametros.setFolioExpediente(vo.getFolioExpediente() == null ? null : vo.getFolioExpediente().toString());
+		parametros.setNumCliente(vo.getNumCliente().toString() == null ? null : vo.getNumCliente().toString());
+		return parametros;
+	}
     
+	public Parametros toParametros() {
+		return ParametrosVO.toParametros(this);
+	}
 }
