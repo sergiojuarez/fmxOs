bf214f7e31db761c924f0ee0ff14d54951d9ba27
Squall Leonhart <leonxleonhart@live.com.mx>
2015-07-21 17:14:59 -0500

    Se agrega OmniSuiteException

---
+++ omnisuite/omnisuite-core/src/main/java/com/finmex/omnisuite/exceptions/OmnisuiteException.java
@@ -0,0 +1,22 @@
+package com.finmex.omnisuite.exceptions;
+
+public class OmnisuiteException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OmnisuiteException() {
+		super();
+	}
+
+	public OmnisuiteException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public OmnisuiteException(String message) {
+		super(message);
+	}
+
+	
+}
