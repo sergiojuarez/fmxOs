bf7e7e49ab26caf51fbbf4d9abcde281ad1eeadc
Uriel Sánchez <uriel.sanchez@planetmedia.com..mx>
2015-07-24 12:58:33 -0500

    regresa el vo en lugar del autogenerado

--- omnisuite/omnisuite-core/src/main/java/com/finmex/omnisuite/credito/movimiento/client/service/CreditoMovimientosClientService.java
+++ omnisuite/omnisuite-core/src/main/java/com/finmex/omnisuite/credito/movimiento/client/service/CreditoMovimientosClientService.java
@@ -1,10 +1,10 @@
 package com.finmex.omnisuite.credito.movimiento.client.service;
 
-import com.finmex.omnisuite.client.MovimientosCredito;
+import com.finmex.omnisuite.credito.movimientos.vo.MovimientosCreditoVO;
 import com.finmex.omnisuite.credito.movimientos.vo.ParametrosVO;
 import com.finmex.omnisuite.exceptions.OmnisuiteException;
 
 public interface CreditoMovimientosClientService {
 	
-	MovimientosCredito consultarUltimosMovimientos(ParametrosVO parametrosVO) throws OmnisuiteException;
+	MovimientosCreditoVO consultarUltimosMovimientos(ParametrosVO parametrosVO) throws OmnisuiteException;
 }
--- omnisuite/omnisuite-core/src/main/java/com/finmex/omnisuite/credito/movimiento/client/service/impl/CreditoMovimientosClientServiceImpl.java
+++ omnisuite/omnisuite-core/src/main/java/com/finmex/omnisuite/credito/movimiento/client/service/impl/CreditoMovimientosClientServiceImpl.java
@@ -1,19 +1,20 @@
 package com.finmex.omnisuite.credito.movimiento.client.service.impl;
 
 import java.net.URL;
+import java.util.ArrayList;
 
-import javax.enterprise.context.Dependent;
-import javax.enterprise.inject.Alternative;
-import javax.enterprise.inject.Default;
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
 
 import com.finmex.omnisuite.client.CreditoMovimientos;
 import com.finmex.omnisuite.client.CreditoMovimientosSoap;
+import com.finmex.omnisuite.client.Movimientos;
 import com.finmex.omnisuite.client.MovimientosCredito;
 import com.finmex.omnisuite.client.ObjectFactory;
 import com.finmex.omnisuite.client.Parametros;
 import com.finmex.omnisuite.credito.movimiento.client.service.CreditoMovimientosClientService;
+import com.finmex.omnisuite.credito.movimientos.vo.MovimientosCreditoVO;
+import com.finmex.omnisuite.credito.movimientos.vo.MovimientosVO;
 import com.finmex.omnisuite.credito.movimientos.vo.ParametrosVO;
 import com.finmex.omnisuite.exceptions.OmnisuiteException;
 
@@ -24,8 +25,8 @@ public class CreditoMovimientosClientServiceImpl implements CreditoMovimientosCl
     String serviceName = "CreditoMovimientos";
     QName serviceQN = new QName(namespace, serviceName);
     Service service;
-	
-	public MovimientosCredito consultarUltimosMovimientos(ParametrosVO parametrosVO) throws OmnisuiteException {
+    
+	public MovimientosCreditoVO consultarUltimosMovimientos(ParametrosVO parametrosVO) throws OmnisuiteException {
 		MovimientosCredito movs = null;
 		ObjectFactory objectFactory = new ObjectFactory();
 		try {
@@ -34,9 +35,14 @@ public class CreditoMovimientosClientServiceImpl implements CreditoMovimientosCl
 			service = CreditoMovimientos.create(new URL(wsdlURL), serviceQN);
 			CreditoMovimientosSoap port = service.getPort(CreditoMovimientosSoap.class);
 			movs = port.movimientosArray(parametros);
+			MovimientosCreditoVO movsVO = new MovimientosCreditoVO();
+			movsVO.setMovimientos(new ArrayList<MovimientosVO>());
+			for(Movimientos movimientos : movs.getMovimientos()){
+				movsVO.getMovimientos().add(new MovimientosVO(movimientos));
+			}
+			return movsVO;
 		} catch (Exception e) {
 			throw new OmnisuiteException("Error al consultar el movimiento de crédito", e);
 		}
-		return movs;
 	}
 }
