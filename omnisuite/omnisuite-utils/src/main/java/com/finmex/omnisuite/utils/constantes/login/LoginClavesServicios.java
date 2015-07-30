ec5b1d65f6064bf0afed63c7e3be79f10d340c32
fecruzg <felipe.cruz@planetmedia.com.mx>
2015-07-30 14:03:43 -0500

    LoginClavesServicios 	-Contiene las claves de los servicios de login.

---
+++ omnisuite/omnisuite-utils/src/main/java/com/finmex/omnisuite/utils/constantes/login/LoginClavesServicios.java
@@ -0,0 +1,18 @@
+package com.finmex.omnisuite.utils.constantes.login;
+
+public final class LoginClavesServicios {
+	
+	private LoginClavesServicios() {}
+	
+	public static final String LOGIN_PATH = "login";
+	public static final String VALIDAR_USUARIO = "loc01";
+	public static final String VALIDAR_CLAVE_ACCESO = "loc02";
+	public static final String ASIGNAR_NUEVA_CLAVE_ACCESO = "lot01";
+	public static final String ASIGNAR_RESPUESTA_SECRETA = "lot02";
+	public static final String ASIGNAR_IMG_SECRETA = "lot03";
+	public static final String ACTIVAR_TOKEN = "lot04";
+	public static final String ACEPTAR_TERMINOS_CONDICIONES = "lot05";
+	public static final String ASIGNAR_MEDIO_NOTIFICACION = "lot06";
+	public static final String ASIGNAR_ALIAS = "lot07";
+	public static final String ASIGAR_PAPERLESS = "lot08";
+}
