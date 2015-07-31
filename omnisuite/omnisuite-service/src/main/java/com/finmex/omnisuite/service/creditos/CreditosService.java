70974f47e0c0a76b7c04e04c9a888b6b49e55f30
SquallxLeonhart <leonxleonhart@live.com.mx>
2015-07-31 01:25:57 -0500

    Se agrega CDI
    Se añade configuración de CDI.

--- omnisuite/omnisuite-service/src/main/java/com/finmex/omnisuite/service/creditos/CreditosService.java
+++ omnisuite/omnisuite-service/src/main/java/com/finmex/omnisuite/service/creditos/CreditosService.java
@@ -1,6 +1,6 @@
 package com.finmex.omnisuite.service.creditos;
 
-import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -12,7 +12,6 @@ import javax.ws.rs.core.Response;
 import org.apache.log4j.Logger;
 
 import com.finmex.omnisuite.credito.movimiento.client.service.CreditoMovimientosClientService;
-import com.finmex.omnisuite.credito.movimiento.client.service.impl.CreditoMovimientosClientServiceImpl;
 import com.finmex.omnisuite.credito.movimientos.vo.MovimientosCreditoVO;
 import com.finmex.omnisuite.credito.movimientos.vo.ParametrosVO;
 import com.finmex.omnisuite.utils.constantes.ConstantesOmnisuite;
@@ -22,28 +21,27 @@ import com.finmex.omnisuite.vo.RespuestaVO;
 /**
  * Recurso que expone servicios REST para el módulo de Créditos.
  * 
- * @author
+ * 
+ * @author Daniel García
  * @since
  */
-@RequestScoped
 @Path(CreditosClavesServicios.CREDITOS_PATH)
 @Produces(ConstantesOmnisuite.APPLICATION_JSON_UTF8)
 @Consumes(MediaType.APPLICATION_JSON)
-public class CreditosConsultaService {
-	private static final Logger LOG = Logger.getLogger(CreditosConsultaService.class);
+public class CreditosService {
+	private static final Logger LOG = Logger.getLogger(CreditosService.class);
 	
-	// @Inject
+	@Inject
 	private CreditoMovimientosClientService creditoMovimientosClientService;
 
 	@POST
 	@Path(CreditosClavesServicios.CONSULTAR_MOVIMIENTOS)
 	public Response consultarUltimosMovimientos(final ParametrosVO parametrosVO) {
-		creditoMovimientosClientService = new CreditoMovimientosClientServiceImpl();
-
 		MovimientosCreditoVO movimientosCreditoVO = creditoMovimientosClientService
 				.consultarUltimosMovimientos(parametrosVO);
 
-		RespuestaVO<MovimientosCreditoVO> respuestaVO = new RespuestaVO<>(Response.Status.ACCEPTED.getStatusCode(),
+		RespuestaVO<MovimientosCreditoVO> respuestaVO = 
+				new RespuestaVO<>(Response.Status.ACCEPTED.getStatusCode(),
 				movimientosCreditoVO);
 
 		return Response.ok(new GenericEntity<RespuestaVO<MovimientosCreditoVO>>(respuestaVO) {}).build();
